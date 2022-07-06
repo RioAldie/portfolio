eddcc3475f10aff51e98d0806b3d3670e2fd3cd4
rioaldie <rioaldierwanto@gmail.com>
2022-07-06 21:02:48 +0700

    refactor skill set

--- pages/component/organism/skill/index.tsx
+++ pages/component/organism/skill/index.tsx
@@ -34,181 +34,72 @@ const Skill = () =>{
                     <Image src={"/icon/kuda.svg"} width={24} height={26}></Image>
                     <Typography variant="h4" fontFamily={'Roboto'} color={'#F2F609'}>My Skills</Typography>
                 </Box>
-                <Box display ='flex' flex={0.6} sx={{marginTop: 10, flexDirection: {xs: 'column', md:'row'}, justifyContent: {xs: 'center', md: 'center'}}}>
+                <Box display ='flex' flex={0.4} sx={{marginTop: 10, flexDirection: {xs: 'column', md:'row'}, justifyContent: {xs: 'center', md: 'center'}}}>
 
-               
-                <Grid container spacing={2} sx={{display: 'flex', justifyContent: {xs: "center", md: "center"}}}>
-                    <Grid item xs={12} md={6}>
-                    <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-                    <Image src={"/image/js-logo.png"} height={40} width={40}></Image>
+                <Grid container spacing={2} sx={{display: 'flex',minHeight: '400px',width: {sm:'100%',md: '60%'}, justifyContent: "space-around",flexWrap:'wrap'}}>
+                    
+                    <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center',ml: 5}}>
+                        <Image src={"/image/html.png"} height={40} width={40}></Image>
+                       <Typography  variant="h6" sx={{marginLeft: 2}}>
+                        HTML
+                       </Typography> 
+                    </Box>
+                    <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center',ml: 5}}>
+                        <Image src={"/icon/css.svg"} height={40} width={40}></Image>
+                       <Typography  variant="h6" sx={{marginLeft: 2}}>
+                        CSS
+                       </Typography> 
+                    </Box>
+                    <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center',ml: 5}}>
+                        <Image src={"/icon/mui.svg"} height={40} width={40}></Image>
+                       <Typography  variant="h6" sx={{marginLeft: 2}}>
+                        Material UI
+                       </Typography> 
+                    </Box>
+                    <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center',ml: 5}}>
+                        <Image src={"/icon/sass.svg"} height={40} width={40}></Image>
+                       <Typography  variant="h6" sx={{marginLeft: 2}}>
+                        SASS
+                       </Typography> 
+                    </Box>
+                    <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center',ml: 5}}>
+                        <Image src={"/image/js-logo.png"} height={40} width={40}></Image>
                        <Typography  variant="h6" sx={{marginLeft: 2}}>
                         Javascript
                        </Typography> 
                     </Box>
-                    
-                    <Demo>
-                        <List dense={dense}>
-                        
-                            <ListItem>
-                                <ListItemText
-                                    primary="Javascript ES6"
-                                    secondary={secondary ? 'Secondary text' : null}
-                                />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText
-                                    primary="DOM Javascript"
-                                    secondary={secondary ? 'Secondary text' : null}
-                                />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText
-                                    primary="Async Function"
-                                    secondary={secondary ? 'Secondary text' : null}
-                                />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText
-                                    primary="Fetch API"
-                                    secondary={secondary ? 'Secondary text' : null}
-                                />
-                            </ListItem>
-                       
-                        </List>
-                    </Demo>
-                    </Grid>
-              
-                    <Grid item xs={12} md={6}>
-                    <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                    <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center',ml: 5}}>
                     <Image src={"/icon/reactjs.svg"} height={40} width={40}></Image>
                        <Typography  variant="h6" sx={{marginLeft: 2}}>
                         ReactJS
                        </Typography> 
                     </Box>
-                    
-                    <Demo>
-                        <List dense={dense}>
-                        
-                            <ListItem>
-                                <ListItemText
-                                    primary="Styled Component"
-                                    secondary={secondary ? 'Secondary text' : null}
-                                />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText
-                                    primary="React Routing"
-                                    secondary={secondary ? 'Secondary text' : null}
-                                />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText
-                                    primary="Context API"
-                                    secondary={secondary ? 'Secondary text' : null}
-                                />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText
-                                    primary="Consume API"
-                                    secondary={secondary ? 'Secondary text' : null}
-                                />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText
-                                    primary="Redux"
-                                    secondary={secondary ? 'Secondary text' : null}
-                                />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText
-                                    primary="Material UI"
-                                    secondary={secondary ? 'Secondary text' : null}
-                                />
-                            </ListItem>
-                       
-                        </List>
-                    </Demo>
-                    </Grid>
                 
-                    <Grid item xs={12} md={6}>
-                    <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                    <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center',ml: 5}}>
+                    <Image src={"/icon/mongodb.svg"} height={40} width={40}></Image>
+                       <Typography  variant="h6" sx={{marginLeft: 2}}>
+                        MongoDB
+                       </Typography> 
+                    </Box>
+                    
+                    
+                
+                    <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center',ml: 5}}>
                     <Image src={"/icon/next.svg"} height={40} width={40}></Image>
                        <Typography  variant="h6" sx={{marginLeft: 2}}>
                         NextJS
                        </Typography> 
                     </Box>
                     
-                    <Demo>
-                        <List dense={dense}>
-                        
-                            <ListItem>
-                                <ListItemText
-                                    primary="Client Side"
-                                    secondary={secondary ? 'Secondary text' : null}
-                                />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText
-                                    primary="Consume API"
-                                    secondary={secondary ? 'Secondary text' : null}
-                                />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText
-                                    primary="React Ecosystem"
-                                    secondary={secondary ? 'Secondary text' : null}
-                                />
-                            </ListItem>
-                       
-                        </List>
-                    </Demo>
-                    </Grid>
-                
-                    <Grid item xs={12} md={6}>
-                    <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                    
+
+                    <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center',ml: 5}}>
                     <Image src={"/image/node.png"} height={40} width={40}></Image>
                        <Typography  variant="h6" sx={{marginLeft: 2}}>
                         NodeJs
                        </Typography> 
                     </Box>
                     
-                    <Demo>
-                        <List dense={dense}>
-                        
-                            <ListItem>
-                                <ListItemText
-                                    primary="CRUD with Mysql"
-                                    secondary={secondary ? 'Secondary text' : null}
-                                />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText
-                                    primary="CRUD with MonggoDB"
-                                    secondary={secondary ? 'Secondary text' : null}
-                                />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText
-                                    primary="Making API"
-                                    secondary={secondary ? 'Secondary text' : null}
-                                />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText
-                                    primary="MVC Concept"
-                                    secondary={secondary ? 'Secondary text' : null}
-                                />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText
-                                    primary="Framework ExpressJS"
-                                    secondary={secondary ? 'Secondary text' : null}
-                                />
-                            </ListItem>
-                       
-                        </List>
-                    </Demo>
-                    </Grid>
                 </Grid>
                 </Box>
         </BoxStyled>
